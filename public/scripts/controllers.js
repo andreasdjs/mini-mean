3d87ebb9efe2ea86017392c13cf920ae80112d85
andreasdjs <andreas.dagnell@gmail.com>
2016-04-15 19:49:40 +0200

    Added basic Angular structure.

---
+++ public/scripts/controllers.js
@@ -0,0 +1,68 @@
+'use strict';
+
+
+app.controller('myController', function($scope, $http, $compile) {
+
+	$scope.hello = function () {
+		console.log("tittut");
+	}
+
+	$scope.cars = [
+	 	{
+	 		"license" : "ABC123",
+	 		"brand" : "Volvo",
+	 		"model" : "Z5000",
+	 		"year" :  "2050",
+			"visibleBrandInput" : false,
+			"visibleYearInput" : false,
+			"visibleModelInput" : false,
+	 		"visibleLicenseInput" : false
+	 	},
+	 	{
+	 		"license" : "XYZ777",
+	 		"brand" : "Volvo",
+	 		"model" : "Z5000",
+	 		"year" :  "2050",
+			"visibleBrandInput" : false,
+			"visibleYearInput" : false,
+			"visibleModelInput" : false,
+	 		"visibleLicenseInput" : false
+	 	},
+
+	 	{
+	 		"license" : "TNY666",
+	 		"brand" : "Volvo",
+	 		"model" : "Z5000",
+	 		"year" :  "2050",
+			"visibleBrandInput" : false,
+			"visibleYearInput" : false,
+			"visibleModelInput" : false,
+	 		"visibleLicenseInput" : false
+	 	},
+	 	{
+	 		"license" : "GPS111",
+	 		"brand" : "Volvo",
+	 		"model" : "Z5000",
+	 		"year" :  "2050",
+			"visibleBrandInput" : false,
+			"visibleYearInput" : false,
+			"visibleModelInput" : false,
+	 		"visibleLicenseInput" : false
+	 	},
+	 ];
+
+	 $scope.save = function (car, index) {
+	 	console.log("Save triggered!");
+	 	console.log(car.brand);
+	 	console.log(car.model);
+	 	console.log(car.year);
+	 	console.log(car.license);
+	 	console.log(index);
+//	 	$scope.cars[index].brand = car.brand;
+	 }
+
+});
+
+
+
+
