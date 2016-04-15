9c5ac789d35193add2007c6f4a7ab1583e6519ab
andreasdjs <andreas.dagnell@gmail.com>
2016-04-15 15:55:02 +0200

    Corrected model location.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var express = require('express');
-var Animal = require('../models/animal'); // eller vad vår model-fil heter
+//var Animal = require('../models/animal'); // eller vad vår model-fil heter
+
+var Animal = require('../models/model');
 
 var router = express.Router();
 
